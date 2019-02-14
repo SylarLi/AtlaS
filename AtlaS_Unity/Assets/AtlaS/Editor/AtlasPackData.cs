5a9193e1223e649c5bc4a51042e7497fb8dde6fa
SylarLi <328403302@qq.com>
2019-02-14 16:29:05 +0800

    override sprite

---
+++ AtlaS_Unity/Assets/AtlaS/Editor/AtlasPackData.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace UnityEditor.UI.Atlas
+{
+    [Serializable]
+    public class AtlasPackData
+    {
+        public int maxAtlasSize;
+
+        public int padding;
+
+        public bool isPOT;
+
+        public bool forceSquare;
+
+        public bool removeFragmentWhenPackingOver;
+
+        public AtlasPackData()
+        {
+            maxAtlasSize = 1024;
+            padding = 1;
+            isPOT = true;
+            forceSquare = false;
+            removeFragmentWhenPackingOver = false;
+        }
+
+        public AtlasPackData(int maxAtlasSize, int padding, bool isPOT, bool forceSquare, bool removeFragmentWhenPackingOver)
+        {
+            this.maxAtlasSize = maxAtlasSize;
+            this.padding = padding;
+            this.isPOT = isPOT;
+            this.forceSquare = forceSquare;
+            this.removeFragmentWhenPackingOver = removeFragmentWhenPackingOver;
+        }
+
+        public AtlasPackData(AtlasPackData data)
+        {
+            maxAtlasSize = data.maxAtlasSize;
+            padding = data.padding;
+            isPOT = data.isPOT;
+            forceSquare = data.forceSquare;
+            removeFragmentWhenPackingOver = data.removeFragmentWhenPackingOver;
+        }
+    }
+}
---
+++ AtlaS_Unity/Assets/AtlaS/Editor/AtlasPackSprite.cs
@@ -0,0 +1,158 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI.Atlas;
+
+namespace UnityEditor.UI.Atlas
+{
+    public class AtlasPackSprite
+    {
+        public static AtlasPackSprite ParseSpriteRaw(BinRaw bin, SpriteRaw sprite)
+        {
+            return new AtlasPackSprite()
+            {
+                main = bin.main,
+                addition = bin.addition,
+                rawQuality = bin.quality,
+                transparency = bin.transparency,
+                quality = bin.quality,
+                name = sprite.name,
+                rect = sprite.rect,
+                border = sprite.border,
+                pivot = sprite.pivot,
+            };
+        }
+
+        public static AtlasPackSprite[] ListSprites(AtlasRaw atlas)
+        {
+            var list = new List<AtlasPackSprite>();
+            foreach (var bin in atlas.bins)
+            {
+                foreach (var sprite in bin.sprites)
+                {
+                    list.Add(ParseSpriteRaw(bin, sprite));
+                }
+            }
+            return list.ToArray();
+        }
+
+        private string mPath;
+
+        public string path
+        {
+            get
+            {
+                return mPath;
+            }
+        }
+
+        private string mName;
+
+        public string name
+        {
+            get
+            {
+                if (!string.IsNullOrEmpty(mName))
+                    return mName;
+                else if (main != null)
+                    return main.name;
+                return "undefined";
+            }
+            set
+            {
+                mName = value;
+            }
+        }
+
+        private Texture2D mMain;
+
+        public Texture2D main
+        {
+            get
+            {
+                if (mMain == null &&
+                    !string.IsNullOrEmpty(path))
+                {
+                    mMain = new Texture2D(2, 2);
+                    mMain.LoadImage(File.ReadAllBytes(path));
+                }
+                return mMain;
+            }
+            set
+            {
+                mMain = value;
+            }
+        }
+
+        private Rect mRect;
+
+        public Rect rect
+        {
+            get
+            {
+                if (mRect.width == 0 ||
+                    mRect.height == 0)
+                {
+                    if (main != null)
+                    {
+                        return new Rect(0, 0, main.width, main.height);
+                    }
+                    return default(Rect);
+                }
+                return mRect;
+            }
+            set
+            {
+                mRect = value;
+            }
+        }
+
+        private int mTransparency = -1;
+        
+        /// <summary>
+        /// if exist alpha channel.
+        /// </summary>
+        public bool transparency
+        {
+            get
+            {
+                if (mTransparency == -1 && 
+                    main != null)
+                {
+                    return AtlasRawUtil.CheckTextureTranparency(main.format);
+                }
+                return mTransparency <= 0;
+            }
+            set
+            {
+                mTransparency = value ? 0 : 1;
+            }
+        }
+
+        /// <summary>
+        /// Raw quality type
+        /// </summary>
+        public BinRaw.Quality rawQuality { get; set; }
+
+        /// <summary>
+        /// Target quality type
+        /// </summary>
+        public BinRaw.Quality quality { get; set; }
+
+        public Texture2D addition { get; set; }
+
+        public Vector4 border { get; set; }
+
+        public Vector2 pivot { get; set; }
+
+        public AtlasPackSprite()
+        {
+
+        }
+
+        public AtlasPackSprite(string path)
+        {
+            mPath = path;
+        }
+    }
+}
